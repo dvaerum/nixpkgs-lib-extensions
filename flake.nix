a7dc10c56d3304360acfa4b64e49faaef1a8457a
Dennis Vestergaard Værum <github@varum.dk>
2026-01-30 04:24:01 +0100

    Added extendLib helper

--- flake.nix
+++ flake.nix
@@ -3,16 +3,23 @@
 
   outputs = { nixpkgs, ...}:
   let
+
     myLib = import ./lib { inherit (nixpkgs) lib; };
+
   in {
     lib = myLib;
+
+    # Keep overlay for pkgs.lib (works in some contexts)
     overlays.default = final: prev: {
       lib = prev.lib.recursiveUpdate prev.lib myLib;
     };
+
+    # Helper for consumers
+    extendLib = lib: nixpkgs.lib.recursiveUpdate lib myLib;
   };
 }
 
 # Test:
-# nix repl --impure --expr 'builtins.getFlake (toString ./.).lib'
+# nix repl --impure --expr '(builtins.getFlake (toString ./.)).outputs'
 # nix repl --impure --expr '(import <nixpkgs> { overlays = [(builtins.getFlake (toString ./.)).overlays.default]; }).lib'
 
